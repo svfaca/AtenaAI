cc139860fc0eea86a41ef2ae13742900f00da7dd
svfaca <savioemmanuelsc@gmail.com>
2026-01-27 01:18:39 -0300

    Update guard.js

--- frontend/js/guard.js
+++ frontend/js/guard.js
@@ -3,5 +3,5 @@ import { isAuthenticated } from "./auth.js";
 console.log("GUARD TOKEN:", localStorage.getItem("access_token"));
 
 if (!isAuthenticated()) {
-  window.location.href = "login.html";
+  window.location.href = "login";
 }
