62aca357695263a363b81dc692626b9599746976
svfaca <savioemmanuelsc@gmail.com>
2026-01-26 23:45:16 -0300

    Update API base URL for production

--- frontend/js/login.js
+++ frontend/js/login.js
@@ -1,7 +1,7 @@
 import { saveToken, saveUserData } from "./auth.js";
 
 // URL do Backend
-const API_BASE_URL = "http://127.0.0.1:8000";
+NEXT_PUBLIC_API_URL=https://web-production-110f3.up.railway.app/
 
 // ========================================================
 // 🎨 FUNÇÃO SHOW TOAST (COM DESIGN)
